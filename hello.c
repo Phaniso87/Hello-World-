b21dec19ae7012480c5f769564935f92cdd6f711
rtayosop <59965746+rtayosop@users.noreply.github.com>
2020-01-16 20:04:49 -0600

    Update hello.c

--- hello.c
+++ hello.c
@@ -10,5 +10,7 @@ printf( "Phaniso Shaba");
 printf("Computer science");
 printf("Malachi Hood");
 printf("Construction Engineering");
+printf("Raoul Tayo Sop");
+printf("Computer Engineering");
 return 0;
 }
